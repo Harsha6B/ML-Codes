126e248c23e842e9dbd09e5d61088279dfa1dec2
Harsha B <harshab.cs23@bmsce.ac.in>
2026-04-21 19:35:01 +0530

    Create linear & multiple linear.py

---
+++ linear & multiple linear.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+df = pd.read_csv("your_dataset.csv")
+
+X = pd.get_dummies(df.iloc[:, :-1])
+y = df.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
