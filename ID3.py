e82890322a4a13815f4a9091130df779df072ef0
Harsha B <harshab.cs23@bmsce.ac.in>
2026-04-21 19:33:53 +0530

    Implement decision tree classifier with evaluation metrics

---
+++ ID3.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import *
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("your_dataset.csv")
+
+X = pd.get_dummies(df.iloc[:, :-1])
+y = df.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier(criterion='entropy')
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average='weighted', zero_division=0))
+print("Recall:", recall_score(y_test, y_pred, average='weighted'))
+print("F1:", f1_score(y_test, y_pred, average='weighted'))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+plt.figure()
+plot_tree(model, filled=True)
+plt.show()
